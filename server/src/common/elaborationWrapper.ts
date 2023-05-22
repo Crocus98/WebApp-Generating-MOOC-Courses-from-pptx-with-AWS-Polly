6eb6a7e8016e482536c2100f1b0ef8e496598007
jack <giacomo.vinati@mail.polimi.it>
2023-05-22 18:45:04 +0200

    elaborationWrapper

--- server/src/common/elaborationWrapper.ts
+++ server/src/common/elaborationWrapper.ts
@@ -1,15 +1,31 @@
-
+import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
+import { LogType } from "@aws-sdk/client-lambda";
+import config from "@config";
 
 class ElaborationWrapper {
     private static elaborationWrapper?: ElaborationWrapper;
+    private lambdaClient: LambdaClient;
 
     constructor() {
-        // TODO
+        this.lambdaClient = new LambdaClient({ region: config.AWS_CONFIG.S3_BUCKET_REGION });
     }
 
     async elaborateFile(email: string) {
         // TODO
     }
+
+    public async invoke (funcName:string, payload:string): Promise<{logs: string, result: string }>{
+        const command = new InvokeCommand({
+          FunctionName: funcName,
+          Payload: JSON.stringify(payload),
+          LogType: LogType.Tail,
+        });
+      
+        const { Payload, LogResult } = await this.lambdaClient.send(command);
+        const result = Buffer.from(Payload).toString();
+        const logs = Buffer.from(LogResult, "base64").toString();
+        return { logs, result };
+      };
 
 
     static getInstance(): ElaborationWrapper {
@@ -17,3 +33,5 @@ class ElaborationWrapper {
         return this.elaborationWrapper;
     }
 }
+
+export default ElaborationWrapper.getInstance();
