18c8876b63a3d74ff430a279aa25bbb656c310ff
Matteo Savino <matteosavino98@gmail.com>
2023-07-27 17:31:20 +0200

    Migration cont'd 12

--- server/src/services/PreviewService.ts
+++ server/src/services/PreviewService.ts
@@ -7,12 +7,13 @@ import elaborationWrapper from "@elaboration-wrapper";
 import { Project } from "@prisma/client";
 import { GetObjectCommandOutput } from "@aws-sdk/client-s3";
 import PreviewException from "@/exceptions/PreviewException";
+import MicroserviceException from "@/exceptions/MicroserviceException";
 
 export const elaborateAudioPreview = async (text: string) => {
     try {
         return await elaborationWrapper.elaborateAudioPreview(text);
     } catch (error) {
-        if (error instanceof LambdaException) {
+        if (error instanceof MicroserviceException) {
             throw new PreviewException(error.message);
         }
         throw new PreviewException("Unexpected error. Could not elaborate preview.");
@@ -23,7 +24,7 @@ export const elaborateSlidesPreview = async (email: string, projectName: string)
     try {
         return await elaborationWrapper.elaborateSlidesPreview(email, projectName);
     } catch (error) {
-        if (error instanceof LambdaException) {
+        if (error instanceof MicroserviceException) {
             throw new PreviewException(error.message);
         }
         else if (error instanceof StorageException) {
