3ea472d4b25247d8ce455e00cd23a96fecd7b625
Matteo Savino <matteosavino98@gmail.com>
2023-09-27 18:48:08 +0200

    Small fix

--- core/ssml_validation.py
+++ core/ssml_validation.py
@@ -6,8 +6,10 @@ import re
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
-schema_path = os.getenv('schema_path')
+schema_filename = os.getenv('schema_file_name')
+
 
 def correct_special_characters(ssml_text):
     escape_chars = {
@@ -19,22 +21,27 @@ def correct_special_characters(ssml_text):
     }
     # Remove leading white spaces
     ssml_text = ssml_text.lstrip()
+
     # Parse the XML document
     root = ET.fromstring(ssml_text)
+
     # Walk through all elements in the document
     for element in root.iter():
         # If the element has a text content, replace the special characters
         if element.text:
             element.text = ''.join(escape_chars.get(c, c)
                                    for c in element.text)
+
         # Also check the tail text
         if element.tail:
             element.tail = ''.join(escape_chars.get(c, c)
                                    for c in element.tail)
 
+
     # Write the corrected document back to a string
     corrected_ssml = ET.tostring(root, encoding='unicode')
     return corrected_ssml
+
 
 
 def has_non_whitespace_text(element):
@@ -46,25 +53,30 @@ def has_non_whitespace_text(element):
     return False
 
 
+
 def validate_ssml(ssml_text):
     ssml_text = ssml_text.lstrip()
     ssml_text = re.sub(r'\s+', ' ', ssml_text).strip()
     try:
-        with open(schema_path, 'r') as schema_file:
+        print(os.getcwd() + "\\" +  schema_filename)
+        with open(os.getcwd() + "\\" + schema_filename, 'r') as schema_file:
             schema_root = etree.parse(schema_file)
             schema = etree.XMLSchema(schema_root)
     except Exception as e:
         raise ElaborationException(
             f"Schema File Exception: {e}")
 
+
     # Correct and parse SSML document
     ssml_text = correct_special_characters(ssml_text)
     ssml = etree.fromstring(ssml_text)
+
 
     # Validate SSML document against schema
     if not schema.validate(ssml):
         raise UserParameterException(
             f"SSML validation failed, check slide: {schema.error_log}")
+
 
     # Additional validationSSML validation failed, check slide
     text_tags = ['p', 's']
@@ -73,6 +85,7 @@ def validate_ssml(ssml_text):
             if not has_non_whitespace_text(element):
                 raise UserParameterException(
                     f"SSML validation failed, check slide")
+
 
 
 def escape_ssml_chars(text):
@@ -84,13 +97,17 @@ def escape_ssml_chars(text):
     return ''.join(escape_chars.get(c, c) for c in text)
 
 
+
 def parse_ssml(ssml_text):
     ssml_text = ssml_text.lstrip()
     lines = deque()
+    
     # Parse the XML document
     root = ET.fromstring(ssml_text)
+
     # Find all 'voice' elements
     voice_elements = root.findall('.//voice')
+
     # If no voice elements are present, create a default one
     if not voice_elements:
         lines.append(('Brian', ssml_text))
@@ -100,14 +117,17 @@ def parse_ssml(ssml_text):
 
             # Start constructing a new 'speak' SSML string
             speak_ssml = "<speak>"
+            
             # If the 'voice' tag contains text, add it to the 'speak' SSML string
             if voice_element.text and voice_element.text.strip():
+
                 # Replace the non-breaking space characters with regular spaces
                 # Also replace newline and tab characters with spaces
-                speak_ssml += voice_element.text.strip().replace('\xa0', ' ').replace('\n',
-                                                                                      ' ').replace('\t', ' ').replace("\\'", "'")
+                speak_ssml += voice_element.text.strip().replace('\xa0', ' ').replace('\n',' ').replace('\t', ' ').replace("\\'", "'")
+
             # Add all child elements of the 'voice' tag to the 'speak' SSML string
             for child in voice_element:
+
                 # Replace the non-breaking space characters with regular spaces
                 # Also replace newline and tab characters with spaces
                 child_ssml = ET.tostring(child, encoding='unicode').replace(
@@ -116,6 +136,7 @@ def parse_ssml(ssml_text):
             speak_ssml += "</speak>"
             lines.append((voice_name, speak_ssml))
     return lines
+
 
 
 def find_missing_tags(ssml_text):
@@ -127,8 +148,8 @@ def find_missing_tags(ssml_text):
 
     # Case 2: '<speak>' tag is present but '<voice>' tag is not
     elif '<speak>' in ssml_text and '<voice voice_name' not in ssml_text:
-        speak_ssml = ssml_text.replace('<speak>', f'<speak><voice voice_name="{voice_name}">').replace(
-            '</speak>', f'</voice></speak>')
+        speak_ssml = ssml_text.replace('<speak>', f'<speak><voice voice_name="{voice_name}">').replace('</speak>', f'</voice></speak>')
+
 
     # Case 3: '<voice>' tag is present but '<speak>' tag is not
     elif '<voice voice_name' in ssml_text and '<speak>' not in ssml_text:
@@ -139,6 +160,7 @@ def find_missing_tags(ssml_text):
         speak_ssml = ssml_text
 
     return speak_ssml
+
 
 # def test_functions():
 #     ssml_text = ''' <speak>
@@ -219,48 +241,36 @@ def find_missing_tags(ssml_text):
 #   </voice>
 # </speak>
 # '''
-
 #     print("Original SSML: \n", ssml_text)
-
 #     checked_missing_tags = find_missing_tags(ssml_text)
 #     print("Checked missing tags: \n", checked_missing_tags)
-
 #     # # Test special character correction function
 #     corrected_ssml = correct_special_characters(checked_missing_tags)
 #     print("Corrected SSML: \n", corrected_ssml)
-
 #     # # Test SSML validation function
 #     validation_result = validate_ssml(corrected_ssml, schema_path)
 #     print("Validation result: ", validation_result)
-
 #     # # Test SSML parsing function
 #     parsed_ssml = parse_ssml(corrected_ssml)
 #     print("plain: ", parsed_ssml)
 #     print("lenght: ", len(parsed_ssml))
-
 #     total_speak_count = 0
-
 #     for voice_name, text in parsed_ssml:
 #         #     # Parse the XML document
 #         root = ET.fromstring(text)
-
 #     #     # Count all 'speak' elements
 #         speak_count = len(root.findall('.//speak'))
-
 #     #     # Add the count to the total
 #         total_speak_count += speak_count
-
 #     print("speak counts: ", total_speak_count)
-
 #     print("Parsed SSML: ")
-
 #     # # while parsed_ssml:
 #     # #     voice_name, text = parsed_ssml.popleft()
 #     # #     print(f"{voice_name}: {text}")
-
 #     for voice_name, text in parsed_ssml:
 #         print(f"{voice_name}: {text}")
 
-
 # if __name__ == "__main__":
+
 #     test_functions()
+
