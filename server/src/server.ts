c98b437c1920698791f4c011355660c9afea4607
aiacoella <angeloiacoella@gmail.com>
2023-11-02 17:43:11 +0100

    cors fix proposition

--- server/src/server.ts
+++ server/src/server.ts
@@ -24,10 +24,16 @@ class Server {
 
   private includeConfig() {
     this.app.use(cors()); // Enable CORS
-    this.app.use(connectLogger(expressLogger, { level: config.LOG_LEVEL.levelStr }));
+    this.app.use(
+      connectLogger(expressLogger, { level: config.LOG_LEVEL.levelStr })
+    );
     const sizeLimit = "20mb";
     // Helmet helps you secure your Express apps by setting various HTTP headers. It's not a silver bullet, but it can help!
     this.app.use(helmet());
+    this.app.use((req, res, next) => {
+      res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+      next();
+    });
     this.app.use(express.json({ limit: sizeLimit }));
     this.app.use(express.urlencoded({ limit: sizeLimit, extended: true }));
     this.app.disable("x-powered-by");
